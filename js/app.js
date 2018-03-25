fd768a5f169fe8c30e1ca7c879d9d0a626353ba5
Zanini, Jonas (jz361t) <jz361t@att.com>
2018-03-25 10:31:49 -0300

    Minor changes
    Working on js files

--- js/app.js
+++ js/app.js
@@ -10,3 +10,18 @@ GAME RULES:
 */
 
 
+var scores, roundScore, activePlayer, dice;
+
+scores = [0,0];
+roundScore = 0;
+activePlayer = 0;
+
+dice = Math.floor(Math.random() * 6) + 1;
+
+console.log(dice);
+
+
+// if (document.querySelector('current') != null){
+document.getElementById('current-0').textContent = dice;
+// }
+//document.querySelectorAll('#current-' + activePlayer).textContent = dice;
